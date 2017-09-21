1b97ce576813876ec48d982f791803b0e4e831eb
zymseo <1047832475@qq.com>
2017-09-21 19:07:13 +0800

    初次创建并提交

---
+++ js/dragging.js
@@ -0,0 +1,54 @@
+/**
+ * Description: 弹出框等元素拖拽类
+ * User: zhaoyiming
+ * Date: 2017/9/21
+ */
+
+;(function(global, oDoc, factory){
+
+	typeof exports === 'object' && typeof module !== 'undefined'
+	? module.exports = factory(oDoc)
+	: typeof define === 'function' && define.amd
+	? define([], function () {
+		return factory(oDoc);
+	})
+	: (global.dragging = factory(oDoc));
+
+})(window, document, function (oDoc) {
+	'use strict';
+
+	function Dragging (opt) {
+		this.opt = opt;
+	}
+
+	Dragging.prototype.init = function () {
+		var opt = this.opt,
+			targetObj = oDoc.querySelector(opt.targetObj), // 鼠标按下的目标dom对象
+			movingObj = oDoc.querySelector(opt.movingObj); // 外部移动的dom对象
+
+		targetObj.style.cssText = 'cursor: move';
+
+		targetObj.onmousedown = function(ev){
+			var ev = ev || global.event,
+				l = ev.clientX - movingObj.offsetLeft,
+				t = ev.clientY - movingObj.offsetTop;
+			
+			oDoc.onmousemove = function(ev){
+				var ev = ev || global.event;
+				movingObj.style.left = ( ev.clientX - l ) + 'px';
+				movingObj.style.top = ( ev.clientY - t ) + 'px';
+			};
+			
+			oDoc.onmouseup = function(){
+				this.onmousemove = null;
+				this.onmouseup = null;
+			};
+		};
+	};
+
+	function dragging (opt) {
+		new Dragging(opt).init();
+	}
+
+	return dragging;
+});
